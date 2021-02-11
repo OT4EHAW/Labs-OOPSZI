c2a70001ad0bc9051aaa92972539a26fb5363f1d
OT4EHAW <78928902+OT4EHAW@users.noreply.github.com>
2021-02-11 23:16:18 +0300

    Add files via upload

---
+++ Task8/task8_lib/task8_lib/task8_func.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "task8_func.h"
+
+using namespace std;
+double x;
+double result;
+
+void f(void) {
+
+	result = 1 / (sqrt(x + 2));
+
+}
---
+++ task7/task7/task7_func.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "task7.h"
+
+using namespace std;
+double x;
+double result;
+
+void f(void) {
+
+	result = 1 / (sqrt(x + 2));
+
+}
